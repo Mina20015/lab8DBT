c1d010ac18dc7e325841fe6fcb98ac44e1677747
mgabdelm@purdue.edu <mgabdelm@purdue.edu>
2025-03-05 18:23:42 +0000

    adding a few files

---
+++ models/lab8/stg_user_activity.sql
@@ -0,0 +1,10 @@
+WITH raw_activity AS (
+    SELECT 
+        user_id, 
+        movie_id, 
+        action, 
+        rating, 
+        timestamp
+    FROM {{ source('public', 'user_activity') }}
+)
+SELECT * FROM raw_activity
